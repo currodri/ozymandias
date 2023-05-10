Fixing bug in gradient for cells with diff ilevel

ozy/phase_diagrams.py
@@ -354,24 +354,26 @@ def plot_single_phase_diagram(pd,field,name,weightvar='cumulative',logscale=True
     print(field,np.nanmin(z).in_units(plotting_z['units']),np.nanmax(z).in_units(plotting_z['units']))
     sim_z = pd.obj.simulation.redshift
     if logscale:
-        if field not in symlog_variables:
+        if field in symlog_variables:
             plot = ax.pcolormesh(x,y,
-                                z[:,:,0].in_units(plotting_z['units']).T,
+                                z[:,:,0].in_units(plotting_z['units']).T.d,
                                 shading='auto',
                                 cmap=plotting_z['cmap'],
-                                norm=LogNorm(vmin=plotting_z['vmin'],
-                                vmax=plotting_z['vmax']))
+                                norm=SymLogNorm(linthresh=plotting_z['linthresh'],
+                                linscale=plotting_z['linscale'],
+                                vmin=plotting_z['vmin_galaxy'],
+                                vmax=plotting_z['vmax_galaxy'],
+                                base=10))
         else:
             plot = ax.pcolormesh(x,y,
-                                z[:,:,0].in_units(plotting_z['units']).T,
+                                z[:,:,0].in_units(plotting_z['units']).T.d,
                                 shading='auto',
                                 cmap=plotting_z['cmap'],
-                                norm=SymLogNorm(vmin=plotting_z['vmin'],
-                                vmax=plotting_z['vmax'],linthresh=plotting_z['linthresh'],
-                                linscale=plotting_z['linscale'],))
+                                norm=LogNorm(vmin=plotting_z['vmin_galaxy'],
+                                vmax=plotting_z['vmax_galaxy']))
     else:
         plot = ax.pcolormesh(x,y,
-                            z[:,:,0].in_units(plotting_z['units']).T,
+                            z[:,:,0].in_units(plotting_z['units']).T.d,
                             shading='auto',
                             cmap=plotting_z['cmap'],
                             vmin=plotting_z['vmin'],
@@ -558,7 +560,7 @@ def plot_compare_phase_diagram(pds,field,name,weightvar='cumulative',
             code_units_z = get_code_units(field)
             z = np.array(pd.zdata['hydro'][field_indexes[ipd]][:,:,weight_indexes[ipd],0].d,order='F')
             z = pd.obj.array(z,code_units_z)
-            print(np.nanmin(z).to('erg'),np.nanmax(z).to('erg'))
+            print(field,np.nanmin(z).to(plotting_z['units']),np.nanmax(z).to(plotting_z['units']))
             sim_z = pd.obj.simulation.redshift
 
             if gent:

ozy/projections.py
@@ -791,18 +791,16 @@ def plot_single_galaxy_projection(proj_FITS,fields,logscale=True,scalebar=(3,'kp
     from matplotlib.colors import LogNorm,SymLogNorm
     from mpl_toolkits.axes_grid1.inset_locator import inset_axes
     import seaborn as sns
-    sns.set(style="dark")
-    plt.rcParams["axes.axisbelow"] = False
     from unyt import unyt_quantity,unyt_array
     import scipy as sp                                                                                                                                                                                     
     import scipy.ndimage                                                                                                                                                                                   
-    # plt.rc('text', usetex=True)
-    # plt.rc('font', family='serif')
-    # hfont = {'fontname':'Helvetica'}
-    # matplotlib.rc('text', usetex = True)
-    # matplotlib.rc('font', **{'family' : "serif"})
-    # params= {'text.latex.preamble' : [r'\usepackage{amsmath}']}
-    # matplotlib.rcParams.update(params)
+    sns.set(style="dark")
+    plt.rcParams["axes.axisbelow"] = False
+    plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+    "font.serif": "Computer Modern Roman",
+    })
 
     if type_scale != '':
         type_scale = '_'+type_scale
@@ -924,16 +922,16 @@ def plot_single_galaxy_projection(proj_FITS,fields,logscale=True,scalebar=(3,'kp
                                 origin='lower',extent=ex,interpolation='nearest',
                                 vmin=plotting_def['vmin'+type_scale],vmax=plotting_def['vmax'+type_scale])
 
-            cbaxes = inset_axes(ax[i,j], width="80%", height="5%", loc='lower center')
+            cbaxes = inset_axes(ax[i,j], width="80%", height="6%", loc='lower center')
             cbar = fig.colorbar(plot, cax=cbaxes, orientation='horizontal')
             if logscale and fields[ivar].split('/')[1] not in symlog_variables:
-                cbar.set_label(plotting_def['label_log'],color=plotting_def['text_over'],fontsize=22,labelpad=-27, y=0.85,weight='bold')
+                cbar.set_label(plotting_def['label_log'],color=plotting_def['text_over'],fontsize=12,labelpad=-27, y=0.85,weight='bold')
             else:
-                cbar.set_label(plotting_def['label'],color=plotting_def['text_over'],fontsize=22,labelpad=-27, y=0.85)
-            cbar.ax.xaxis.label.set_font_properties(matplotlib.font_manager.FontProperties(weight='bold',size=8))
-            cbar.ax.tick_params(axis='x', pad=-7, labelsize=8,labelcolor=plotting_def['text_over'])
+                cbar.set_label(plotting_def['label'],color=plotting_def['text_over'],fontsize=12,labelpad=-27, y=0.85)
+            cbar.ax.tick_params(axis='x', pad=-8.05, labelsize=11,labelcolor=plotting_def['text_over'])
             cbar.ax.tick_params(length=0,width=0)
             cbar.outline.set_linewidth(1)
+            cbar.ax.set_axisbelow(False)
             invert_tick_colours(cbar.ax,full_varname,type_scale)
 
             if redshift and i==0 and j==0:

tests/test_proj.py
@@ -9,69 +9,69 @@ virial_mass = [i.virial_quantities['mass'] for i in obj.galaxies]
 progind = np.argmax(virial_mass)
 gal = obj.galaxies[progind]
 
-# proj = do_projection(gal,['gas/density','gas/temperature','star/sdensity',
-#                           'gas/neighbour_accuracy','gas/grad_crp','gas/eff_FKmag'],
-#                             window=(5,'kpc'),pov='faceon',
-#                             filter_conds=['none','star_age/<=/0.01/Myr'],
-#                             filter_name=['all','young'],remove_subs=True)
-# proj.save_FITS('NUT_00035_faceon.fits')
-# plot_single_galaxy_projection('NUT_00035_faceon.fits',['gas/density','gas/temperature','star/sdensity',
-#                                                   'gas/neighbour_accuracy','gas/grad_crp','gas/eff_FKmag'],
-#                               filter_name='young')
-# plot_single_galaxy_projection('NUT_00035_faceon.fits',['gas/density','gas/temperature','star/sdensity',
-#                                                   'gas/neighbour_accuracy','gas/grad_crp','gas/eff_FKmag'],
-#                               filter_name='all')
+proj = do_projection(gal,['gas/density','gas/temperature','star/sdensity',
+                          'gas/neighbour_accuracy','gas/grad_crp','gas/eff_FKmag'],
+                            window=(5,'kpc'),pov='faceon',
+                            filter_conds=['none','star_age/<=/0.01/Myr'],
+                            filter_name=['all','young'],remove_subs=True)
+proj.save_FITS('NUT_00035_faceon.fits')
+plot_single_galaxy_projection('NUT_00035_faceon.fits',['gas/density','gas/temperature','star/sdensity',
+                                                  'gas/neighbour_accuracy','gas/grad_crp','gas/eff_FKmag'],
+                              filter_name='young')
+plot_single_galaxy_projection('NUT_00035_faceon.fits',['gas/density','gas/temperature','star/sdensity',
+                                                  'gas/neighbour_accuracy','gas/grad_crp','gas/eff_FKmag'],
+                              filter_name='all')
 
-# proj = do_projection(gal,['gas/density','gas/temperature','star/sdensity','dm/sdensity'],
-#                             window=(5,'kpc'),pov='edgeon')
-# proj.save_FITS('NUT_00035_edgeon.fits')
-# plot_single_galaxy_projection('NUT_00035_edgeon.fits',['gas/density','gas/temperature',
-#                                                        'star/sdensity','dm/sdensity'])
+proj = do_projection(gal,['gas/density','gas/temperature','star/sdensity','dm/sdensity'],
+                            window=(5,'kpc'),pov='edgeon')
+proj.save_FITS('NUT_00035_edgeon.fits')
+plot_single_galaxy_projection('NUT_00035_edgeon.fits',['gas/density','gas/temperature',
+                                                       'star/sdensity','dm/sdensity'])
 
-# proj = do_projection(gal,['gas/density','gas/temperature','star/sdensity',
-#                           'gas/neighbour_accuracy','gas/grad_crp','gas/eff_FKmag'],
-#                             window=(5,'kpc'),pov='z',
-#                             filter_conds=['none','star_age/<=/0.01/Myr'],
-#                             filter_name=['all','young'])
-# proj.save_FITS('NUT_00035_z.fits')
-# plot_single_galaxy_projection('NUT_00035_z.fits',['gas/density','gas/temperature','star/sdensity',
-#                                                   'gas/neighbour_accuracy','gas/grad_crp','gas/eff_FKmag'],
-#                               filter_name='young')
-# plot_single_galaxy_projection('NUT_00035_z.fits',['gas/density','gas/temperature','star/sdensity',
-#                                                   'gas/neighbour_accuracy','gas/grad_crp','gas/eff_FKmag'],
-#                               filter_name='all')
+proj = do_projection(gal,['gas/density','gas/temperature','star/sdensity',
+                          'gas/neighbour_accuracy','gas/grad_crp','gas/eff_FKmag'],
+                            window=(5,'kpc'),pov='z',
+                            filter_conds=['none','star_age/<=/0.01/Myr'],
+                            filter_name=['all','young'])
+proj.save_FITS('NUT_00035_z.fits')
+plot_single_galaxy_projection('NUT_00035_z.fits',['gas/density','gas/temperature','star/sdensity',
+                                                  'gas/neighbour_accuracy','gas/grad_crp','gas/eff_FKmag'],
+                              filter_name='young')
+plot_single_galaxy_projection('NUT_00035_z.fits',['gas/density','gas/temperature','star/sdensity',
+                                                  'gas/neighbour_accuracy','gas/grad_crp','gas/eff_FKmag'],
+                              filter_name='all')
 
-# proj = do_projection(gal,['gas/density','gas/temperature','star/sdensity',
-#                           'gas/neighbour_accuracy','gas/grad_crp','gas/eff_FKmag'],
-#                             window=(5,'kpc'),pov='x',
-#                             filter_conds=['none','star_age/<=/0.01/Myr'],
-#                             filter_name=['all','young'])
-# proj.save_FITS('NUT_00035_x.fits')
-# plot_single_galaxy_projection('NUT_00035_x.fits',['gas/density','gas/temperature','star/sdensity',
-#                                                   'gas/neighbour_accuracy','gas/grad_crp','gas/eff_FKmag'],
-#                               filter_name='young')
-# plot_single_galaxy_projection('NUT_00035_x.fits',['gas/density','gas/temperature','star/sdensity',
-#                                                   'gas/neighbour_accuracy','gas/grad_crp','gas/eff_FKmag'],
-#                               filter_name='all')
-# proj = do_projection(gal,['gas/density','gas/temperature','star/sdensity',
-#                           'gas/magnetic_magnitude','gas/grad_crp','gas/eff_FKmag'],
-#                             window=(5,'kpc'),pov='y')
-# proj.save_FITS('NUT_00035_y.fits')
-# plot_single_galaxy_projection('NUT_00035_y.fits',['gas/density','gas/temperature','star/sdensity',
-#                                                   'gas/magnetic_magnitude','gas/grad_crp','gas/eff_FKmag'])
-# plot_single_galaxy_projection('NUT_00035_y.fits',['gas/density','gas/temperature','star/sdensity',
-                                                  # 'gas/neighbour_accuracy','gas/grad_crp','gas/eff_FKmag'])
-# proj = do_projection(gal,['gas/density','gas/temperature','gas/v_cyl_z','gas/v_sphere_r','gas/grav_crpfz','gas/grav_crpfrsphere','gas/sigma','gas/grav_therpfz'],window=(0.1,'rvir'),pov='edgeon',weight=['gas/density','star/cumulative'])
-# proj.save_FITS('NUT_00035_edgeon.fits')
-plot_single_galaxy_projection('NUT_00035_edgeon.fits',['gas/density','gas/temperature','gas/v_cyl_z','gas/v_sphere_r','gas/grav_crpfz','gas/grav_crpfrsphere','gas/sigma','gas/grav_therpfz'],type_scale='galaxy')
+proj = do_projection(gal,['gas/density','gas/temperature','star/sdensity',
+                          'gas/neighbour_accuracy','gas/grad_crp','gas/eff_FKmag'],
+                            window=(5,'kpc'),pov='x',
+                            filter_conds=['none','star_age/<=/0.01/Myr'],
+                            filter_name=['all','young'])
+proj.save_FITS('NUT_00035_x.fits')
+plot_single_galaxy_projection('NUT_00035_x.fits',['gas/density','gas/temperature','star/sdensity',
+                                                  'gas/neighbour_accuracy','gas/grad_crp','gas/eff_FKmag'],
+                              filter_name='young')
+plot_single_galaxy_projection('NUT_00035_x.fits',['gas/density','gas/temperature','star/sdensity',
+                                                  'gas/neighbour_accuracy','gas/grad_crp','gas/eff_FKmag'],
+                              filter_name='all')
+proj = do_projection(gal,['gas/density','gas/temperature','star/sdensity',
+                          'gas/magnetic_magnitude','gas/grad_crp','gas/eff_FKmag'],
+                            window=(5,'kpc'),pov='y')
+proj.save_FITS('NUT_00035_y.fits')
+plot_single_galaxy_projection('NUT_00035_y.fits',['gas/density','gas/temperature','star/sdensity',
+                                                  'gas/magnetic_magnitude','gas/grad_crp','gas/eff_FKmag'])
+plot_single_galaxy_projection('NUT_00035_y.fits',['gas/density','gas/temperature','star/sdensity',
+                                                  'gas/neighbour_accuracy','gas/grad_crp','gas/eff_FKmag'])
+proj = do_projection(gal,['gas/density','gas/temperature','gas/v_cyl_z','gas/v_sphere_r','gas/alfvendiff_ratio','gas/grav_crpfrsphere','gas/sigma','gas/grav_therpfz'],window=(0.1,'rvir'),pov='edgeon',weight=['gas/density','star/cumulative'])
+proj.save_FITS('NUT_00035_edgeon.fits')
+plot_single_galaxy_projection('NUT_00035_edgeon.fits',['gas/density','gas/temperature','gas/v_cyl_z','gas/v_sphere_r','gas/alfvendiff_ratio','gas/grav_crpfrsphere','gas/sigma','gas/grav_therpfz'],type_scale='galaxy')
 
-# proj = do_projection(gal,['gas/density','gas/temperature','gas/eff_FKmag','gas/magnetic_magnitude','gas/streaming_heating','gas/grad_crp'],window=(1,'kpc'),pov='faceon')
-# proj.save_FITS('NUT_00035_faceon.fits')
-# plot_single_galaxy_projection('NUT_00035_faceon.fits',['gas/density','gas/temperature','gas/eff_FKmag','gas/magnetic_magnitude','gas/streaming_heating','gas/grad_crp'])
+proj = do_projection(gal,['gas/density','gas/temperature','gas/eff_FKmag','gas/magnetic_magnitude','gas/streaming_heating','gas/grad_crp'],window=(1,'kpc'),pov='faceon')
+proj.save_FITS('NUT_00035_faceon.fits')
+plot_single_galaxy_projection('NUT_00035_faceon.fits',['gas/density','gas/temperature','gas/eff_FKmag','gas/magnetic_magnitude','gas/streaming_heating','gas/grad_crp'])
 
-# proj = do_healpix_projection(gal,['gas/massflow_rate_sphere_r','gas/density','gas/temperature','gas/metallicity',
-#                                     'gas/magnetic_energy_specific','gas/cr_energy_specific'],
-#                                     r=(0.2,'rvir'),dr=(0.01,'rvir'),weight=['gas/absmomentum_sphere_r','star/mass'],
-#                                     remove_subs=True)
-# proj.save_FITS('NUT_00035_mollweide.fits')
-# plot_single_galaxy_healpix('NUT_00035_mollweide.fits',['gas/massflow_rate_sphere_r','gas/density','gas/temperature','gas/metallicity','gas/magnetic_energy_specific','gas/cr_energy_specific'])
+proj = do_healpix_projection(gal,['gas/massflow_rate_sphere_r','gas/density','gas/temperature','gas/metallicity',
+                                    'gas/magnetic_energy_specific','gas/cr_energy_specific'],
+                                    r=(0.2,'rvir'),dr=(0.01,'rvir'),weight=['gas/absmomentum_sphere_r','star/mass'],
+                                    remove_subs=True)
+proj.save_FITS('NUT_00035_mollweide.fits')
+plot_single_galaxy_healpix('NUT_00035_mollweide.fits',['gas/massflow_rate_sphere_r','gas/density','gas/temperature','gas/metallicity','gas/magnetic_energy_specific','gas/cr_energy_specific'])